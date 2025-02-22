b1d8078a2d260db07c821392456e2a5816913366
Esha Vigneswaran <eshavigneswaran@gmail.com>
2025-02-21 22:08:21 -0500

    interactive plot

--- app.py
+++ app.py
@@ -1,25 +1,25 @@
-import streamlit as st 
+import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
+import plotly.express as px
+
 
 df2 = pd.read_csv('race.csv')
-df2.plot(x='X.1', y=['White', 'Hispanic', 'Black', 'Asian'], kind='line', marker='o', title='Percentage of Mortality Rate vs. Race')
-plt.xlabel('Year')
-plt.ylabel('Percentage of Mortality Rate')
-plt.xticks(range(int(df2['X.1'].min()), int(df2['X.1'].max()) + 1))
 
-col1, col2 = st.columns([1, 1])
-with col2:  # Put the plot in the left column
-    st.pyplot(plt)
-
-#st.pyplot(plt)
+fig1 = px.line(df2, x='X.1', y=['White', 'Hispanic', 'Black', 'Asian'], 
+               markers=True, title='Percentage of Mortality Rate vs. Race')
+fig1.update_layout(xaxis_title='Year', yaxis_title='Percentage of Mortality Rate')
 
 df3 = pd.read_csv('age.csv')
-df3.plot(x='Year', y=['Under 25', '25-39', '40 and Older'], kind='line', marker='o', title='Percentage of Mortality Rate vs Age')
-plt.xlabel('Year')
-plt.ylabel('Percentage of Mortality Rate')
-plt.xticks(range(int(df3['Year'].min()), int(df3['Year'].max()) + 1))
 
-with col1:  # Put the plot in the left column
-    st.pyplot(plt)
+fig2 = px.line(df3, x='Year', y=['Under 25', '25-39', '40 and Older'], 
+               markers=True, title='Percentage of Mortality Rate vs. Age')
+fig2.update_layout(xaxis_title='Year', yaxis_title='Percentage of Mortality Rate')
+
+# Use columns to arrange graphs
+col1, col2 = st.columns(2)
+
+with col1:
+    st.plotly_chart(fig1, use_container_width=True)
+
+with col2:
+    st.plotly_chart(fig2, use_container_width=True)
