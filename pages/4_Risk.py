69fb7889169f651f7301472e1d40b725d7e27d25
bhavyat-01 <148793084+bhavyat-01@users.noreply.github.com>
2025-02-22 23:54:16 -0500

    Updated 4_Risk.py

--- pages/4_Risk.py
+++ pages/4_Risk.py
@@ -10,6 +10,9 @@ def predictRisk():
     # Load the model
     model = tf.keras.models.load_model('risk_predictor.h5')  # Replace with your model file name
 
+    
+    st.title("Predict Your Risk for Complications during Pregnancy")
+    st.text("Enter simple information such as age, blood pressure, blood sugar, etc. to see how risky your pregnancy may be. Take a look at the suggestions below to reduce your risk.")
     with st.form("my_form"):
         age = st.text_input("Enter Age")
         SBP = st.text_input("Enter Systolic BP")
@@ -31,5 +34,7 @@ def predictRisk():
             if(predicted_class == 2):
                 st.text("You are at high risk")
             
+            response = get_gemini_response("UNDER 200 WORDS: What can I do if I am at "+risk_level+" risk for maternal mortality? Specifically age wise.")
+            st.markdown("To prevent any unfortunate event, "+response)
     
 predictRisk()
