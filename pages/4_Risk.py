c6a3f73bc511801a99786354899ec3e6f13fe069
Bhavya Tanugula <bhavya.tanugula@gmail.com>
2025-02-22 22:26:34 -0500

    Incorporated ML into project

--- pages/4_Risk.py
+++ pages/4_Risk.py
@@ -1,18 +1,35 @@
 import streamlit as st
 import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import models
-from tensorflow.keras.models import load_model
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+
 
 
 def predictRisk(): 
-    st.title("Hey")
 
     # Load the model
-    model = load_model('my_model.h5')  # Replace with your model file name
+    model = tf.keras.models.load_model('risk_predictor.h5')  # Replace with your model file name
 
-    # Use the model for predictions
-    predictions = model.predict(input_data)
-    
+    with st.form("my_form"):
+        age = st.text_input("Enter Age")
+        SBP = st.text_input("Enter Systolic BP")
+        DBP = st.text_input("Enter Diastolic BP")
+        BS = st.text_input("Enter Blood Sugar")
+        BT = st.text_input("Enter Body Temperature")
+        HR = st.text_input("Enter Heart Rate")
+        button = st.form_submit_button("Submit Information")
+
+        if button:
+            # Use the model for predictions
+            predictions = model.predict(np.array([[int(age), int(SBP), int(DBP), int(BS), int(BT), int(HR)]]))
+            predicted_class = tf.argmax(predictions, axis=1).numpy()[0]  # Get the predicted class
+            
+            if(predicted_class == 0):
+                st.text("You are at low risk")
+            if(predicted_class == 1):
+                st.text("You are at medium risk")
+            if(predicted_class == 2):
+                st.text("You are at high risk")
+            
     
 predictRisk()
